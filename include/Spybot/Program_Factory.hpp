a463c1d064e0190a49779bcf409d3dfd0f24b780
Spectra Twilight <SpectraRTwilight@gmail.com>
2019-07-31 18:16:10 -0500

    Program_Factories can now be generated by providing a DataLoader object that is currently storing program data.

--- include/Spybot/Program_Factory.hpp
+++ include/Spybot/Program_Factory.hpp
@@ -7,12 +7,22 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <Spybot/DataLoader.hpp>
+
 namespace Spybot
 {
     class Program_Factory
-    {
-        //=======================================================================================================================
+    {   //=======================================================================================================================
         private:
+        // Placeholder data, in the case that externally provided data is invalid.
+        static std::string error_name;
+        static std::string error_description;
+        const static uint8_t error_maxSize = 1;
+        const static uint8_t error_moveSpeed = 0;
+        static std::string error_icon;
+        static sf::Color error_color;
+
+        //-----------------------------------------------------------------------------------------------------------------------
         // Identifying information.
         std::string name;
         std::string description;
@@ -24,11 +34,39 @@ namespace Spybot
 
         // Graphics
         sf::Texture icon;
-        sf::Color primaryColor; // Known as 'color1' in the data files.
-        sf::Color secondaryColor; // Known as 'color2' in the data files.
+        sf::Color color;
 
         //=======================================================================================================================
         public:
+        /*-----------------------------------------------------------------------------------------------------------------------
+            Constructs a Program_Factory with no defined attributes.
+        */
+        inline Program_Factory() {}
+
+        /*-----------------------------------------------------------------------------------------------------------------------
+            Constructs a Program_Factory defined by the attributes stored within 
+            the provided DataLoader at the time of the call to this constructor.
+
+            If the required attributes are not currently available from the DataLoader,
+            then substitutions will be made using placeholder values.
+
+            Parameter:
+            *   loader: DataLoader with attributes for defining Programs currently loaded.
+        */
+        inline Program_Factory(const DataLoader& loader) { define(loader); }
+
+        /*-----------------------------------------------------------------------------------------------------------------------
+            Defines the attributes of this Program_Factory using the attributes 
+            stored within the provided DataLoader at the time of the call.
+
+            If the required attributes are not currently available from the DataLoader,
+            then substitutions will be made using placeholder values.
+
+            Parameter:
+            *   loader: DataLoader with attributes for defining Programs currently loaded.
+        */
+        void define(const DataLoader& loader);
+
         //-----------------------------------------------------------------------------------------------------------------------
         // Getter Methods
         inline const std::string& getName() const { return name; }
@@ -39,10 +77,20 @@ namespace Spybot
         inline const std::list<std::string>& getCommands() const { return commands; }
 
         inline const sf::Texture& getIcon() const { return icon; }
-        inline const sf::Color& getPrimaryColor() const { return primaryColor; }
-        inline const sf::Color& getSecondaryColor() const { return secondaryColor; }
+        inline const sf::Color& getColor() const { return color; }
         //-----------------------------------------------------------------------------------------------------------------------
-    };
+        // Setter Methods
+        inline void setName(const std::string& newName) { name = newName; }
+        inline void setDescription(const std::string& newDesc) { description = newDesc; }
+
+        inline void setMaxSize(uint8_t newSize) { maxSize = newSize; }
+        inline void setMoveSpeed(uint8_t newSpeed) { moveSpeed = newSpeed; }
+        inline void addCommand(const std::string& newCommand) { commands.push_back(newCommand); }
+
+        inline bool loadIcon(const std::string& iconFilepath) { return icon.loadFromFile(iconFilepath); }
+        inline void setColor(const sf::Color& newPrimaryColor) { color = newPrimaryColor; }
+        //-----------------------------------------------------------------------------------------------------------------------
+    };  //=======================================================================================================================
 }
 
 #endif // SPYBOT_PROGRAM_FACTORY_HPP
