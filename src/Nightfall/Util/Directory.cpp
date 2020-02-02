931de0893e4fd1d2fdd149c6e154980085878b8c
Spectra Twilight <SpectraRTwilight@gmail.com>
2020-02-02 00:51:34 -0600

    Rewrote filepath-returning functions to reserve necessary space in the string before construction to reduce overall number of heap allocations.

--- src/Nightfall/Util/Directory.cpp
+++ src/Nightfall/Util/Directory.cpp
@@ -1,4 +1,9 @@
 ////////////////////////////////////////////////////////////////////////////////
+/// Standard Library Includes
+////////////////////////////////////////////////////////////////////////////////
+#include <cstdint>
+
+////////////////////////////////////////////////////////////////////////////////
 /// Nightfall Includes
 ////////////////////////////////////////////////////////////////////////////////
 #include <Nightfall/Util/Directory.hpp>
@@ -8,41 +13,79 @@ namespace nightfall::util
     ////////////////////////////////////////////////////////////////////////////
     std::string Directory::command_data()
     {
-        return std::string(_data)
+        constexpr uint16_t size = _data.size() + _commands.size();
+
+        std::string filepath;
+        filepath.reserve(size);
+
+        (filepath = _data)
             += _commands;
+
+        return filepath;
     }
 
     ////////////////////////////////////////////////////////////////////////////
     std::string Directory::gfx_program_base()
     {
-        return ((std::string(_data)
+        constexpr uint16_t size = 
+            _data.size() + _gfx.size() + _gfx_programs.size() + _gfx_programs_base.size();
+
+        std::string filepath;
+        filepath.reserve(size);
+
+        (((filepath = _data)
             += _gfx)
             += _gfx_programs)
             += _gfx_programs_base;
+
+        return filepath;
     }
 
     ////////////////////////////////////////////////////////////////////////////
     std::string Directory::gfx_program_error()
     {
-        return ((std::string(_data)
+        constexpr uint16_t size =
+            _data.size() + _gfx.size() + _gfx_programs.size() + _gfx_programs_error.size();
+
+        std::string filepath;
+        filepath.reserve(size);
+
+        (((filepath = _data)
             += _gfx)
             += _gfx_programs)
             += _gfx_programs_error;
+
+        return filepath;
     }
 
     ////////////////////////////////////////////////////////////////////////////
     std::string Directory::gfx_sector_default()
     {
-        return ((std::string(_data)
+        constexpr uint16_t size = 
+            _data.size() + _gfx.size() + _gfx_sectors.size() + _gfx_sectors_default.size();
+
+        std::string filepath;
+        filepath.reserve(size);
+
+        (((filepath = _data)
             += _gfx)
             += _gfx_sectors)
             += _gfx_sectors_default;
+
+        return filepath;
     }
 
     ////////////////////////////////////////////////////////////////////////////
     std::string Directory::program_data()
     {
-        return std::string(_data)
+        constexpr uint16_t size = _data.size() + _programs.size();
+
+        std::string filepath;
+        filepath.reserve(size);
+
+        (filepath = _data)
             += _programs;
+
+        return filepath;
     }
 } // namespace nightfall::util
